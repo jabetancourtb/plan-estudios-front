228927fc5fd303ed5f94f8ee4de8adedd244247d
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-06-23 19:54:15 -0500

    Se crean archivos environment para dev, test y prod.

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiPlanEstudiosUDistrital:{
+        apiKey:{
+            requestHeader: "X-API-KEY",
+            requestValue: "H5sStbqNbvKL_29cnzGQFtgf_kWSWTkyjExFg1W_"
+        },
+        url: "http://localhost:9090/api/v1",
+    }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    apiPlanEstudiosUDistrital:{
+        apiKey:{
+            requestHeader: "X-API-KEY",
+            requestValue: "H5sStbqNbvKL_29cnzGQFtgf_kWSWTkyjExFg1W_"
+        },
+        url: "http://localhost:9090/api/v1",
+    }
+}
