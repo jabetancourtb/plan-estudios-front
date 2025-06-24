01dfd23140e1a3838fe3c7f1d17697077bcb499f
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-06-23 19:55:53 -0500

    Se modifica app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AsignaturasComponent } from './pages/asignaturas/asignaturas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'asignaturas',
+        pathMatch: 'full'
+    },
+    {
+        path: 'asignaturas',
+        component: AsignaturasComponent,
+    },
+];
