47c07f4e769b06d4b56f1a607c8e120d5f3b7239
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-06-23 19:53:07 -0500

    Se crean componentes para footer

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent { 
+
+  currentYear = new Date().getFullYear();
+  
+}
