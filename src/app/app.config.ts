943c0c63a14119f769b41fb7f1e8f61bd4fb265c
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-03 21:31:03 -0500

    Se modifican main.ts y app.config.ts
    1. Se realizan importaciones de dependencias de echarts.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), 
+    provideEchartsCore({
+      echarts
+    })
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,23 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+
+import * as echarts from 'echarts/core';
+import {
+  TooltipComponent,
+  VisualMapComponent,
+  DatasetComponent
+} from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+import { CustomChart } from 'echarts/charts';
+
+echarts.use([
+  CanvasRenderer,
+  CustomChart,
+  TooltipComponent,
+  VisualMapComponent,
+  DatasetComponent
+]);
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
