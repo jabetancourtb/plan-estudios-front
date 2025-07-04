7156e5cbb6f03ed6c58852e9157f5609f4ab599a
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-04 17:56:50 -0500

    Se crea componente navbar.component

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent { 
+  
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+}
