8c0566a8d065229ec5daa9b0dc7d98dfbec5dd9f
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-04 23:59:53 -0500

    Se elimina import sin usar en NavbarComponent.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [NavbarComponent],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
