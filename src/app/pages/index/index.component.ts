fc28456b9889f28b45e534b2474af1db6ac9ddaf
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-04 21:41:58 -0500

    Se modifica index.component .ts y .html.

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, inject, signal, SimpleChanges } fro
 import { ActivatedRoute } from '@angular/router';
 import { AsignaturaService } from '../../services/asignatura.service';
 import { Asignatura } from '../../models/asignatura.model';
-import { ResponseList } from '../../dto/response-list.model';
+import { ResponseListDTO } from '../../dto/response-list.model';
 import { BubbleChartComponent } from "../echart/bubble-chart/bubble-chart.component";
 import { CampoFormacionService } from '../../services/campo-formacion.service';
 import { CampoFormacion } from '../../models/campo-formacion.model';
@@ -10,6 +10,8 @@ import { LoaderService } from '../../services/loader.service';
 import { AreaFormacion } from '../../models/area-formacion.model';
 import { AreaFormacionService } from '../../services/area-formacion.service';
 import { CirclePackingComponent } from "../echart/circle-packing/circle-packing.component";
+import { URLParamsDTO } from '../../dto/url-params.model';
+
 
 @Component({
   selector: 'app-index',
@@ -31,23 +33,35 @@ export class IndexComponent {
   areasFormacion = signal<AreaFormacion[]>([]);
   asignaturas = signal<Asignatura[]>([]);
 
-  categoria = signal('');
+  tipoGrafico = signal('');
+  //categoria = signal('');
+  //idCampoFormacionParam = signal(0);
+  //idAreaFormacionParam = signal(0);
 
-  responseListAsignaturas = signal<ResponseList<Asignatura>>({
+  urlParams = signal<URLParamsDTO>({
+    "categoria": '',
+    "idCampoFormacion": 0,
+    "nombreCampoFormacion": '',
+    "idAreaFormacion": 0,
+    "nombreAreaFormacion": '',
+    "idAsignatura": 0
+  })
+
+  responseListAsignaturas = signal<ResponseListDTO<Asignatura>>({
     recordCountPerPage: 0,
     totalRecordCount: 0,
     totalPages: 0,
     content: []
   });
 
-  responseListCamposFormacion = signal<ResponseList<CampoFormacion>>({
+  responseListCamposFormacion = signal<ResponseListDTO<CampoFormacion>>({
     recordCountPerPage: 0,
     totalRecordCount: 0,
     totalPages: 0,
     content: []
   });
 
-  responseListAreasFormacion = signal<ResponseList<AreaFormacion>>({
+  responseListAreasFormacion = signal<ResponseListDTO<AreaFormacion>>({
     recordCountPerPage: 0,
     totalRecordCount: 0,
     totalPages: 0,
@@ -56,30 +70,67 @@ export class IndexComponent {
 
 
   ngOnInit(): void {
-    this.obtenerCategoria();
+    this.obtenerUrlParams();
     this.consultarCategoria();
   }
 
 
-  consultarCategoria() {
-    if (this.categoria() == 'campos-formacion') {
-      this.consultarCamposFormacion(1, 100, undefined, true);
-    }
-    else if(this.categoria() == 'areas-formacion') {
-      this.consultarAreasFormacion(1, 100, undefined, true);
-    }
-    else if(this.categoria() == 'asignaturas') {
-      this.consultarAsignaturas(1, 100, 'codigo', true);
-    }
-  }
+  obtenerUrlParams() {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      const tipoGrafico = params.get('tipoGrafico') ?? '';
+      this.tipoGrafico.set(tipoGrafico);
+    });
 
-
-  obtenerCategoria() {
     this.activatedRoute.queryParamMap.subscribe((params) => {
-      const categoria = params.get('categoria') ?? '';
-      this.categoria.set(categoria);
+      /*
+      this.paramsUrl.update(current => ({
+        ...current,
+        categoria: 'categoria',
+      }));
+
+      const idCampoFormacionParam = params.get('idCampoFormacion') ?? 0;
+      this.idCampoFormacionParam.set(Number(idCampoFormacionParam));
+
+      const idAreaFormacionParam = params.get('idCampoFormacion') ?? 0;
+      this.idAreaFormacionParam.set(Number(idAreaFormacionParam))
+      */
+
+      this.urlParams.set({
+        categoria: params.get('categoria') ?? '',
+        idCampoFormacion: Number(params.get('idCampoFormacion')) ?? 0,
+        nombreCampoFormacion: params.get('nombreCampoFormacion') ?? '',
+        idAreaFormacion: Number(params.get('idAreaFormacion')) ?? 0,
+        nombreAreaFormacion: params.get('nombreAreaFormacion') ?? '',
+        idAsignatura: Number(params.get('idAsignatura')) ?? 0,
+      });
+
     });
   }
+
+
+  consultarCategoria() {
+    if (this.urlParams().categoria == 'campos-formacion') {
+      this.consultarCamposFormacion(1, 100, undefined, true);
+    }
+    else if(this.urlParams().categoria == 'areas-formacion') {
+      if(this.urlParams().idCampoFormacion) {
+        this.consultarAreasFormacionPorIdCampoFormacion(this.urlParams().idCampoFormacion, 1, 100, undefined, true);
+      }
+      else {
+        this.consultarAreasFormacion(1, 100, undefined, true);
+      } 
+    }
+    else if(this.urlParams().categoria == 'asignaturas') {
+      if(this.urlParams().idCampoFormacion && this.urlParams().idAreaFormacion) {
+        this.consultarAsignaturasPorCampoFormacionYAreaFormacion(this.urlParams().nombreCampoFormacion, this.urlParams().nombreAreaFormacion, 1, 100, 'codigo', true);
+      }
+      else {
+        this.consultarAsignaturas(1, 100, 'codigo', true);
+      }
+      
+    }
+  }
+
 
   private consultarCamposFormacion(page?: number, pageSize?: number, field?: string, asc?: boolean) {
     this.loaderService.show();
@@ -94,9 +145,23 @@ export class IndexComponent {
     });
   }
 
+
   private consultarAreasFormacion(page?: number, pageSize?: number, field?: string, asc?: boolean) {
     this.loaderService.show();
     this.areaFormacionService.consultarAreasFormacion(page, pageSize, field, asc).subscribe({
+      next: (res) => {
+        this.responseListAreasFormacion.set(res);
+        this.loaderService.hide();
+      },
+      error: (e) => {
+        this.loaderService.hide();
+      }
+    });
+  }
+
+  private consultarAreasFormacionPorIdCampoFormacion(idCampoFormacion: number, page?: number, pageSize?: number, field?: string, asc?: boolean) {
+    this.loaderService.show();
+    this.areaFormacionService.consultarAreasFormacionPorIdCampoFormacion(idCampoFormacion, page, pageSize, field, asc).subscribe({
       next: (res) => {
         this.responseListAreasFormacion.set(res);
         this.loaderService.hide();
@@ -113,6 +178,7 @@ export class IndexComponent {
     this.asignaturaService.consultarAsignaturasPorCarrera(carrera, 1, 100, 'codigo', true).subscribe({
       next: (res) => {
         this.responseListAsignaturas.set(res);
+        this.loaderService.hide();
       },
       error: (e) => {
         this.loaderService.hide();
@@ -126,6 +192,20 @@ export class IndexComponent {
     this.asignaturaService.consultarAsignaturas(1, 100, 'codigo', true).subscribe({
       next: (res) => {
         this.responseListAsignaturas.set(res);
+        this.loaderService.hide();
+      },
+      error: (e) => {
+        this.loaderService.hide();
+      }
+    });
+  }
+
+  private consultarAsignaturasPorCampoFormacionYAreaFormacion(camporFormacion:string, areaFormacion: string, page: number, pageSize: number, field: string, asc: boolean) {
+    this.loaderService.show();
+    this.asignaturaService.consultarAsignaturasPorCampoFormacionYAreaFormacion(camporFormacion, areaFormacion, 1, 100, 'codigo', true).subscribe({
+      next: (res) => {
+        this.responseListAsignaturas.set(res);
+        this.loaderService.hide();
       },
       error: (e) => {
         this.loaderService.hide();
