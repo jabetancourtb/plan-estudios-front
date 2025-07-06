5ddc16832be8291af8d57f92e423d5ff47d36af4
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-06 08:10:14 -0500

    Se elimina footer.component de index.component

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
-import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 
 @Component({
   selector: 'app-index',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
