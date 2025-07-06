9a442ea50a7132b1f13b14d0c0cee32ee9743fd7
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-05 23:07:34 -0500

    Se ajusta redireccionamientos en navbar.component

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +8,39 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent { 
-  
+export class NavbarComponent {
+
+  router = inject(Router);
+
   menuOpen = false;
+
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+
+  // Redirige a / luego redigecciona a la ruta deseada
+  goToCamposFormacionBubbleChart() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate(['/bubble-chart/campos-formacion']);
+    });
+  }
+
+
+  // Redirige a / luego redigecciona a la ruta deseada
+  goToAreasFormacionBubbleChart() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate(['/bubble-chart/areas-formacion']);
+    });
+  }
+
+
+  // Redirige a / luego redigecciona a la ruta deseada
+  goToAsignaturasBubbleChart() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate(['/bubble-chart/asignaturas']);
+    });
   }
 
 }
