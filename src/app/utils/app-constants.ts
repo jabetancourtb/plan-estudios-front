87ed8730ebb2d807e8931598926a66133146a019
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-06 08:28:05 -0500

    Se crea archivo para guardar constantes de las rutas y demás.

---
+++ src/app/utils/app-constants.ts
@@ -0,0 +1,14 @@
+export const APP_CONSTANTS = {
+  BASE_API_URL: 'https://api.miapp.com',
+
+  ENDPOINTS: {},
+
+  ROUTES: {
+    index: '/index',
+    camposFormacionEchartBubbleChart: '/campos-formacion/echart/bubble-chart',
+    areasFormacionEchartBubbleChart: '/areas-formacion/echart/bubble-chart',
+    asignaturasEchartBubbleChart: '/asignaturas/echart/bubble-chart',
+    planEstudiosEchartCirclePacking: '/plan-estudios/echart/circle-packing',
+  },
+
+};
