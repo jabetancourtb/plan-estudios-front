dbdb49ccf11da0d56339690c491a09e32de783d9
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-06 16:26:00 -0500

    Adds prerequisites tree chart route
    Adds a new route and component for displaying the prerequisites as a tree chart.
    
    This allows users to visualize the prerequisite relationships between different elements.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,32 +4,37 @@ import { AsignaturasBubbleChartComponent } from './pages/asignaturas/bubble-char
 import { CamposFormacionBubbleChartComponent } from './pages/campos-formacion/echart/bubble-chart/campos-formacion-bubble-chart.component';
 import { AreasFormacionBubbleChartComponent } from './pages/areas-formacion/echart/bubble-chart/areas-formacion-bubble-chart.component';
 import { PlanEstudiosCirclePackingComponent } from './pages/plan-estudios/echart/circle-packing/plan-estudios-circle-packing.component';
+import { PrerrequisitosTreeChartComponent } from './pages/prerrequisitos/echart/tree-chart/prerrequisitos-tree-chart/prerrequisitos-tree-chart.component';
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'index',
-        pathMatch: 'full'
+      path: '',
+      redirectTo: 'index',
+      pathMatch: 'full'
     },
     {
-        path: 'index',
-        component: IndexComponent,
+      path: 'index',
+      component: IndexComponent,
     },
     {
-        path: 'campos-formacion/echart/bubble-chart',
-        component: CamposFormacionBubbleChartComponent,
+      path: 'campos-formacion/echart/bubble-chart',
+      component: CamposFormacionBubbleChartComponent,
     },
     {
-        path: 'areas-formacion/echart/bubble-chart',
-        component: AreasFormacionBubbleChartComponent,
+      path: 'areas-formacion/echart/bubble-chart',
+      component: AreasFormacionBubbleChartComponent,
     },
     {
-        path: 'asignaturas/echart/bubble-chart',
-        component: AsignaturasBubbleChartComponent,
+      path: 'asignaturas/echart/bubble-chart',
+      component: AsignaturasBubbleChartComponent,
     },
     {
-        path: 'plan-estudios/echart/circle-packing',
-        component: PlanEstudiosCirclePackingComponent,
+    path: 'plan-estudios/echart/circle-packing',
+    component: PlanEstudiosCirclePackingComponent,
+    },
+    {
+      path: 'prerrequisitos/echart/tree-chart',
+      component: PrerrequisitosTreeChartComponent,
     },
 
 ];
