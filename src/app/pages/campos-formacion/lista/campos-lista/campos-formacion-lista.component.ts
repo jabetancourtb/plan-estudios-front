11fdfe86e7dc566f1562c7cfec46fd2fbe6d4e59
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-09 20:31:48 -0500

    Implements field of formation list view
    Adds the field of formation list view with pagination, search, and page size selection.
    
    This commit introduces a new component to display a list of
    formation fields with the ability to search, paginate, and select
    the number of items displayed per page. It also includes basic
    styling for the table and pagination elements.

---
+++ src/app/pages/campos-formacion/lista/campos-lista/campos-formacion-lista.component.ts
@@ -0,0 +1,109 @@
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoaderService } from '../../../../services/loader.service';
+import { CampoFormacionService } from '../../../../services/campo-formacion.service';
+import { ResponseListDTO } from '../../../../dto/response-list.model';
+import { CampoFormacion } from '../../../../models/campo-formacion.model';
+import { NavbarComponent } from "../../../../shared/components/navbar/navbar.component";
+import { FormsModule } from '@angular/forms';
+import { FilterAllFieldsPipe } from '../../../../pipes/filter-all-fields.pipe';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-campos-formacion-lista',
+  imports: [NavbarComponent, FormsModule, FilterAllFieldsPipe, NgStyle],
+  templateUrl: './campos-formacion-lista.component.html',
+  styleUrl: './campos-formacion-lista.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CamposFormacionListaComponent {
+
+  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private loaderService: LoaderService = inject(LoaderService);
+  private campoFormacionService: CampoFormacionService = inject(CampoFormacionService);
+
+  responseListCamposFormacion = signal<ResponseListDTO<CampoFormacion>>({
+    recordCountPerPage: 0,
+    totalRecordCount: 0,
+    totalPages: 0,
+    content: []
+  });
+
+  campoFormacion = signal<CampoFormacion>({} as CampoFormacion);
+
+  searchTerm = '';
+  currentPage = 1;
+  pageSize = 10;
+  totalPages: number[] = [];
+  pageSizeOptions = [10, 25, 50, 100];
+
+
+  ngOnInit() {
+    this.currentPage = 1;
+    this.consultarQueryParams();
+  }
+
+
+  consultarQueryParams() {
+    this.activatedRoute.queryParams.subscribe(params => {
+
+      this.currentPage = params['page'] ? +params['page'] : 1;
+      this.pageSize = params['pageSize'] ? +params['pageSize'] : 10;
+      const field = params['field'] || 'id';
+      const asc = params['asc'] || true ;
+      this.searchTerm = params['searchTerm'] || '';
+
+      this.consultarCamposformacionPorPaginacion(this.currentPage, this.pageSize, field, asc);
+    });
+  }
+
+
+  private consultarCamposformacionPorPaginacion(page: number, pageSize: number, field: string, asc: boolean) {
+    this.loaderService.show();
+    this.campoFormacionService.consultarCamposFormacion(page, pageSize, field, asc).subscribe({
+      next: (res) => {
+        this.responseListCamposFormacion.set(res);
+        this.updatePageInformation(page);
+        this.loaderService.hide();
+      },
+      error: (e) => {
+        this.loaderService.hide();
+      }
+    });
+  }
+
+
+  updatePageInformation(page: number): void {
+    this.totalPages = Array.from({ length: this.responseListCamposFormacion().totalPages }, (_, i) => i + 1);
+    this.currentPage = page
+  }
+
+
+  updatePageSize(): void {
+    //this.consultarCamposformacionPorPaginacion(1, this.pageSize, 'id', true);
+  }
+
+
+  goToPreviousPage(page: number) {
+    if(page == 0) {
+      return;
+    }
+
+    this.consultarCamposformacionPorPaginacion(page, this.pageSize, 'id', true);
+  }
+
+
+  goToNextPage(page: number) {
+    if(page > this.responseListCamposFormacion().totalPages) {
+      return;
+    }
+
+    this.consultarCamposformacionPorPaginacion(page, this.pageSize, 'id', true);
+  }
+
+
+  goToPage(page: number) {
+    this.consultarCamposformacionPorPaginacion(page, this.pageSize, 'id', true);
+  }
+
+}
