8259c844bf8fbbd90827ce0f2cfc2af02de092a7
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-10 00:36:13 -0500

    Improves asignatura list filtering and UI
    Enhances the asignatura list component by improving filtering capabilities and UI layout.
    
    Moves filter options into a single row for better organization.
    Renames `asc` to `ascending` for clarity.
    Adds a search input for filtering by all fields.
    Updates pagination and record count display for improved user experience.
    
    The changes enhance usability and provide a more streamlined filtering experience for users.

--- src/app/pages/asignaturas/lista/asignaturas-lista/asignaturas-lista.component.ts
+++ src/app/pages/asignaturas/lista/asignaturas-lista/asignaturas-lista.component.ts
@@ -34,20 +34,19 @@ export class AsignaturasListaComponent {
 
   searchTerm = '';
   field = 'codigo';
-  asc = true;
+  ascending = true;
   currentPage = 1;
   pageSize = 10;
   totalPages: number[] = [];
   pageSizeOptions = [10, 25, 50, 100];
   fieldsOptions: string[] = [];
-  ascendOptions = [
+  ascendingOptions = [
     { value: true, label: 'Ascendente' },
     { value: false, label: 'Descendente' }
   ];
 
 
   ngOnInit() {
-    this.currentPage = 1;
     this.consultarQueryParams();
   }
 
@@ -58,10 +57,10 @@ export class AsignaturasListaComponent {
       this.currentPage = params['page'] ? +params['page'] : 1;
       this.pageSize = params['pageSize'] ? +params['pageSize'] : 10;
       this.field = params['field'] || 'codigo';
-      this.asc = params['asc'] || true ;
+      this.ascending = params['ascending'] || true ;
       this.searchTerm = params['searchTerm'] || '';
 
-      this.consultarAsignaturasPorPaginacion(this.currentPage, this.pageSize, this.field, this.asc);
+      this.consultarAsignaturasPorPaginacion(this.currentPage, this.pageSize, this.field, this.ascending);
     });
   }
 
@@ -72,19 +71,20 @@ export class AsignaturasListaComponent {
       next: (res) => {
         this.responseListAsignaturas.set(res);
         this.updatePageInformation(page);
-        this.loadFieldsOptions();
         this.loaderService.hide();
       },
       error: (e) => {
         this.loaderService.hide();
+        this.updatePageInformation(page);
       }
     });
   }
 
 
-  loadFieldsOptions() {
+  loadOptions() {
     this.fieldsOptions = [];
     if(this.responseListAsignaturas().content.length > 0) {
+      this.totalPages = Array.from({ length: this.responseListAsignaturas().totalPages }, (_, i) => i + 1);
       const key = Object.keys(this.responseListAsignaturas().content[0]);
       this.fieldsOptions.push(...key);
     }
@@ -92,14 +92,14 @@ export class AsignaturasListaComponent {
 
 
   updatePageInformation(page: number): void {
-    this.totalPages = Array.from({ length: this.responseListAsignaturas().totalPages }, (_, i) => i + 1);
-    this.currentPage = page
+    this.currentPage = page;
+    this.loadOptions();
+    this.setQueryParams();
   }
 
 
   updateFilters(): void {
-    this.setQueryParams();
-    this.consultarAsignaturasPorPaginacion(1, this.pageSize, this.field, this.asc);
+    this.consultarAsignaturasPorPaginacion(1, this.pageSize, this.field, this.ascending);
   }
 
 
@@ -110,7 +110,7 @@ export class AsignaturasListaComponent {
         page: this.currentPage,
         pageSize: this.pageSize,
         field: this.field,
-        asc: this.asc,
+        ascending: this.ascending,
         searchTerm: this.searchTerm
       },
       queryParamsHandling: 'merge' // para mantener otros parámetros existentes
@@ -123,7 +123,7 @@ export class AsignaturasListaComponent {
       return;
     }
 
-    this.consultarAsignaturasPorPaginacion(page, this.pageSize, this.field, this.asc);
+    this.consultarAsignaturasPorPaginacion(page, this.pageSize, this.field, this.ascending);
   }
 
 
@@ -132,12 +132,12 @@ export class AsignaturasListaComponent {
       return;
     }
 
-    this.consultarAsignaturasPorPaginacion(page, this.pageSize, this.field, this.asc);
+    this.consultarAsignaturasPorPaginacion(page, this.pageSize, this.field, this.ascending);
   }
 
 
   goToPage(page: number) {
-    this.consultarAsignaturasPorPaginacion(page, this.pageSize, this.field, this.asc);
+    this.consultarAsignaturasPorPaginacion(page, this.pageSize, this.field, this.ascending);
   }
 
 
