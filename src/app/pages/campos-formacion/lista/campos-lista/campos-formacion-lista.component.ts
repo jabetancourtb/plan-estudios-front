2684c9913594f77bcada1de813c5c75722221144
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-10 23:01:16 -0500

    Refactors campo formación list to use filter component
    Moves filter and pagination logic into a shared filter component.
    
    This change improves code reusability and simplifies the
    campos formación list component. It encapsulates filtering and
    pagination parameters into a `FilterPaginationDTO` signal.

--- src/app/pages/campos-formacion/lista/campos-lista/campos-formacion-lista.component.ts
+++ src/app/pages/campos-formacion/lista/campos-lista/campos-formacion-lista.component.ts
@@ -8,10 +8,12 @@ import { NavbarComponent } from "../../../../shared/components/navbar/navbar.com
 import { FormsModule } from '@angular/forms';
 import { FilterAllFieldsPipe } from '../../../../pipes/filter-all-fields.pipe';
 import { NgStyle } from '@angular/common';
+import { FilterPaginationDTO } from '../../../../dto/filter-pagination.model';
+import { FilterPaginationComponent } from '../../../../shared/components/filter-pagination/filter-pagination/filter-pagination.component';
 
 @Component({
   selector: 'app-campos-formacion-lista',
-  imports: [NavbarComponent, FormsModule, FilterAllFieldsPipe, NgStyle],
+  imports: [NavbarComponent, FormsModule, FilterAllFieldsPipe, NgStyle, FilterPaginationComponent],
   templateUrl: './campos-formacion-lista.component.html',
   styleUrl: './campos-formacion-lista.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -32,13 +34,7 @@ export class CamposFormacionListaComponent {
 
   campoFormacion = signal<CampoFormacion>({} as CampoFormacion);
 
-  searchTerm = '';
-  field = 'id';
-  ascending = true;
-  currentPage = 1;
-  pageSize = 10;
-  totalPages: number[] = [];
-  pageSizeOptions = [10, 25, 50, 100];
+  filterPaginationDTO = signal<FilterPaginationDTO>(new FilterPaginationDTO());
 
   fieldsOptions = [
     { value: 'id', label: 'Id' },
@@ -48,27 +44,33 @@ export class CamposFormacionListaComponent {
     { value: 'cantidadAsignaturas', label: 'Cantidad de asignaturas' }
   ];
 
-  ascendingOptions = [
-    { value: true, label: 'Ascendente' },
-    { value: false, label: 'Descendente' }
-  ];
-
 
   ngOnInit() {
     this.consultarQueryParams();
   }
 
 
+  // Consulta los query params de la URL
+  // incluso cuando estos cambian.
+  // Se ejecuta siempre después de this.setQueryParams()
   consultarQueryParams() {
     this.activatedRoute.queryParams.subscribe(params => {
 
-      this.currentPage = params['page'] ? +params['page'] : 1;
-      this.pageSize = params['pageSize'] ? +params['pageSize'] : 10;
-      this.field = params['field'] || 'id';
-      this.ascending = params['ascending'] || true ;
-      this.searchTerm = params['searchTerm'] || '';
+      this.filterPaginationDTO.set(new FilterPaginationDTO({
+        currentPage: params['page'] ? +params['page'] : 1,
+        pageSize: params['pageSize'] ? +params['pageSize'] : 10,
+        field: params['field'] || 'id',
+        fieldsOptions: this.fieldsOptions,
+        ascending: params['ascending'] || true ,
+        searchTerm: params['searchTerm'] || ''
+      }));
 
-      this.consultarCamposformacionPorPaginacion(this.currentPage, this.pageSize, this.field, this.ascending);
+      this.consultarCamposformacionPorPaginacion(
+        this.filterPaginationDTO().currentPage,
+        this.filterPaginationDTO().pageSize,
+        this.filterPaginationDTO().field ,
+        this.filterPaginationDTO().ascending
+      );
     });
   }
 
@@ -78,21 +80,24 @@ export class CamposFormacionListaComponent {
     this.campoFormacionService.consultarCamposFormacion(page, pageSize, field, asc).subscribe({
       next: (res) => {
         this.responseListCamposFormacion.set(res);
-        this.updatePageInformation(page);
+        this.updatePageInformation();
         this.loaderService.hide();
       },
       error: (e) => {
-        this.updatePageInformation(page);
+        this.updatePageInformation();
         this.loaderService.hide();
       }
     });
   }
 
 
-  updatePageInformation(page: number): void {
-    this.currentPage = page;
-    this.setQueryParams();
-    this.loadOptions();
+  updatePageInformation(): void {
+    if(this.responseListCamposFormacion().content.length > 0) {
+      this.filterPaginationDTO.set(new FilterPaginationDTO({
+        ...this.filterPaginationDTO(),
+        pages: Array.from({ length: this.responseListCamposFormacion().totalPages }, (_, i) => i + 1),
+      }));
+    }
   }
 
 
@@ -100,35 +105,30 @@ export class CamposFormacionListaComponent {
     this.router.navigate([], {
       relativeTo: this.activatedRoute,
       queryParams: {
-        page: this.currentPage,
-        pageSize: this.pageSize,
-        field: this.field,
-        ascending: this.ascending,
-        searchTerm: this.searchTerm
+        page: this.filterPaginationDTO().currentPage,
+        pageSize: this.filterPaginationDTO().pageSize,
+        field: this.filterPaginationDTO().field,
+        ascending: this.filterPaginationDTO().ascending,
+        searchTerm: this.filterPaginationDTO().searchTerm
       },
       queryParamsHandling: 'merge' // para mantener otros parámetros existentes
     });
   }
 
 
-  loadOptions() {
-    if(this.responseListCamposFormacion().content.length > 0) {
-      this.totalPages = Array.from({ length: this.responseListCamposFormacion().totalPages }, (_, i) => i + 1);
+   filterQuery(event: any): void {
+    this.filterPaginationDTO().currentPage = 1,
+    this.filterPaginationDTO().pageSize = event.pageSize;
+    this.filterPaginationDTO().field = event.field;
+    this.filterPaginationDTO().ascending = event.ascending;
+
+    // Evita ejecutar el servicio de consulta ya que se usa el pipe de filtrado
+    if(event.searchTerm != this.filterPaginationDTO().searchTerm) {
+      this.filterPaginationDTO().searchTerm = event.searchTerm;
+      return;
     }
-  }
 
-
-  updateFilters(): void {
-    this.consultarCamposformacionPorPaginacion(1, this.pageSize, this.field, true);
-  }
-
-
-  cleanFilters() {
-    this.pageSize = 10;
-    this.field = 'id';
-    this.ascending = true;
-    this.searchTerm = '';
-    this.consultarCamposformacionPorPaginacion(1, this.pageSize, this.field, this.ascending);
+    this.setQueryParams();
   }
 
 
@@ -137,7 +137,8 @@ export class CamposFormacionListaComponent {
       return;
     }
 
-    this.consultarCamposformacionPorPaginacion(page, this.pageSize, this.field, this.ascending);
+    this.filterPaginationDTO().currentPage = page;
+    this.setQueryParams();
   }
 
 
@@ -146,12 +147,14 @@ export class CamposFormacionListaComponent {
       return;
     }
 
-    this.consultarCamposformacionPorPaginacion(page, this.pageSize, this.field, this.ascending);
+    this.filterPaginationDTO().currentPage = page;
+    this.setQueryParams();
   }
 
 
   goToPage(page: number) {
-    this.consultarCamposformacionPorPaginacion(page, this.pageSize, this.field, this.ascending);
+    this.filterPaginationDTO().currentPage = page;
+    this.setQueryParams();
   }
 
 }
