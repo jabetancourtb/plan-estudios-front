fc5a57086d439e3112feda777ecc5222a4c15847
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-11 18:04:18 -0500

    Adds functionality to view related data
    Implements functionality to display areas of formation and subjects related to each training field.
    
    Includes buttons in the training field list to open a SweetAlert containing lists of related areas of formation and subjects. These lists provide links to the respective entities.

--- src/app/pages/campos-formacion/lista/campos-lista/campos-formacion-lista.component.ts
+++ src/app/pages/campos-formacion/lista/campos-lista/campos-formacion-lista.component.ts
@@ -11,6 +11,12 @@ import { NgStyle } from '@angular/common';
 import { FilterPaginationDTO } from '../../../../dto/filter-pagination.model';
 import { FilterPaginationComponent } from '../../../../shared/components/filter-pagination/filter-pagination.component';
 import { PaginationComponent } from "../../../../shared/components/pagination/pagination.component";
+import { AreaFormacionService } from '../../../../services/area-formacion.service';
+import { AsignaturaService } from '../../../../services/asignatura.service';
+import { AreaFormacion } from '../../../../models/area-formacion.model';
+import { Asignatura } from '../../../../models/asignatura.model';
+import Swal from 'sweetalert2';
+import { APP_CONSTANTS } from '../../../../utils/app-constants';
 
 @Component({
   selector: 'app-campos-formacion-lista',
@@ -25,6 +31,8 @@ export class CamposFormacionListaComponent {
   private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   private loaderService: LoaderService = inject(LoaderService);
   private campoFormacionService: CampoFormacionService = inject(CampoFormacionService);
+  private areaFormacionService: AreaFormacionService = inject(AreaFormacionService);
+  private asignaturaService: AsignaturaService = inject(AsignaturaService);
 
   responseListCamposFormacion = signal<ResponseListDTO<CampoFormacion>>({
     recordCountPerPage: 0,
@@ -42,8 +50,24 @@ export class CamposFormacionListaComponent {
     { value: 'nombre', label: 'Nombre' },
     { value: 'color', label: 'Color' },
     { value: 'cantidadAreasFormacion', label: 'Cantidad de áreas de Formación' },
-    { value: 'cantidadAsignaturas', label: 'Cantidad de asignaturas' }
+    { value: 'cantidadAsignaturas', label: 'Cantidad de asignaturas' },
+    { value: 'verAreasFormacion', label: 'Ver áreas de formación asignadas' },
+    { value: 'asignaturasAsignadas', label: 'Ver asignaturas asignadas' }
   ];
+
+  responseListAreasFormacion = signal<ResponseListDTO<AreaFormacion>>({
+    recordCountPerPage: 0,
+    totalRecordCount: 0,
+    totalPages: 0,
+    content: []
+  });
+
+  responseListAsignaturas = signal<ResponseListDTO<Asignatura>>({
+    recordCountPerPage: 0,
+    totalRecordCount: 0,
+    totalPages: 0,
+    content: []
+  });
 
 
   ngOnInit() {
@@ -93,7 +117,7 @@ export class CamposFormacionListaComponent {
 
 
   updatePageInformation(): void {
-    if(this.responseListCamposFormacion().content.length > 0) {
+    if(this.responseListCamposFormacion() && this.responseListCamposFormacion().content.length > 0) {
       this.filterPaginationDTO.set(new FilterPaginationDTO({
         ...this.filterPaginationDTO(),
         totalItems: this.responseListCamposFormacion().totalRecordCount,
@@ -141,6 +165,92 @@ export class CamposFormacionListaComponent {
   goToPage(page: number) {
     this.filterPaginationDTO().currentPage = page;
     this.setQueryParams();
+  }
+
+
+  consultarAreasFormacionPorCampoFormacion(campoFormacion: CampoFormacion) {
+    this.loaderService.show();
+    this.areaFormacionService.consultarAreasFormacionPorIdCampoFormacion(campoFormacion.id, 1, 100, 'id', true).subscribe({
+      next: (res) => {
+        this.responseListAreasFormacion.set(res);
+        this.showSwalAreasFormacionAsignadas(campoFormacion, this.responseListAreasFormacion().content);
+        this.loaderService.hide();
+      },
+      error: (e) => {
+        this.loaderService.hide();
+      }
+    });
+  }
+
+
+  showSwalAreasFormacionAsignadas(campoFormacion: CampoFormacion, areasFormacion: AreaFormacion[]) {
+    let html = `
+      <div style="max-height: 300px; overflow-y: auto;">
+        <ul style='list-style: none; padding-left: 0;'>
+    `;
+
+    for (let areaFormacion of areasFormacion) {
+      html += `
+        <li class='mb-3'>
+          <a href="${APP_CONSTANTS.ROUTES.areasFormacionLista}?pageSize=50&searchTerm=${encodeURIComponent(areaFormacion.nombre)}">
+            ${areaFormacion.nombre}
+          </a>
+        </li>
+      `;
+    }
+
+    html += `
+        </ul>
+      </div>
+    `;
+
+    Swal.fire({
+      title: `areas de formación de ${campoFormacion.nombre}`,
+      html: html
+    });
+  }
+
+
+  consultarAsignaturasPorCampoFormacion(campoFormacion: CampoFormacion) {
+    this.loaderService.show();
+    this.asignaturaService.consultarAsignaturasPorCampoFormacion(campoFormacion.nombre, 1, 100, 'codigo', true).subscribe({
+      next: (res) => {
+        this.responseListAsignaturas.set(res);
+        this.showSwalAsignaturasAsignadas(campoFormacion, this.responseListAsignaturas().content);
+        this.loaderService.hide();
+      },
+      error: (e) => {
+        this.loaderService.hide();
+      }
+    });
+  }
+
+
+  showSwalAsignaturasAsignadas(campoFormacion: CampoFormacion, asignaturas: Asignatura[]) {
+    let html = `
+      <div style="max-height: 300px; overflow-y: auto;">
+        <ul style='list-style: none; padding-left: 0;'>
+    `;
+
+    for (let asignatura of asignaturas) {
+      html += `
+        <li class='mb-3'>
+          <a href="${APP_CONSTANTS.ROUTES.asignaturasLista}?pageSize=200&searchTerm=${encodeURIComponent(asignatura.nombre)}">
+            ${asignatura.nombre}
+          </a>
+        </li>
+      `;
+    }
+
+    html += `
+        </ul>
+      </div>
+    `;
+
+    Swal.fire({
+      title: `Asignaturas de ${campoFormacion.nombre}`,
+      html: html
+    });
   }
 
 }
