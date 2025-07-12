80704496f101378875826c7908567d05543e0d06
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-12 15:16:56 -0500

    Adds prerrequisitos-buscar component
    This commit introduces the 'prerrequisitos-buscar' component.
    
    This component will be used to search for prerequisites.

---
+++ src/app/pages/prerrequisitos/buscar/prerrequisitos-buscar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prerrequisitos-buscar',
+  imports: [],
+  templateUrl: './prerrequisitos-buscar.component.html',
+  styleUrl: './prerrequisitos-buscar.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PrerrequisitosBuscarComponent { }
