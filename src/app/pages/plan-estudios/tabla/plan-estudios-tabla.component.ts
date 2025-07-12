b2aabe6eb2c6c5dc50c3c6ac29144ac45e4407ae
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-12 15:16:51 -0500

    Adds plan estudios table component
    Adds the plan estudios table component with basic template and styling.
    This component will display the plan de estudios data in a tabular format.

---
+++ src/app/pages/plan-estudios/tabla/plan-estudios-tabla.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-estudios-tabla',
+  imports: [],
+  templateUrl: './plan-estudios-tabla.component.html',
+  styleUrl: './plan-estudios-tabla.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PlanEstudiosTablaComponent { }
