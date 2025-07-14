8cff084f29d8d06235d1cf8407cce413e749b671
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-14 15:44:46 -0500

    Implements basic diagram component
    Adds a basic diagram component using goJS library.
    
    Includes node and link templates for visual representation.
    Integrates the component within the plan de estudios page.

--- src/app/pages/plan-estudios/tabla/plan-estudios-tabla.component.ts
+++ src/app/pages/plan-estudios/tabla/plan-estudios-tabla.component.ts
@@ -1,10 +1,228 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild  } from '@angular/core';
+import * as go from 'gojs';
+import {
+  DiagramComponent,
+} from 'gojs-angular';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-plan-estudios-tabla',
-  imports: [],
+  imports: [DiagramComponent, CommonModule, NavbarComponent],
   templateUrl: './plan-estudios-tabla.component.html',
   styleUrl: './plan-estudios-tabla.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlanEstudiosTablaComponent { }
+export class PlanEstudiosTablaComponent {
+
+  @ViewChild('myDiagram', { static: true }) public myDiagramComponent!: ElementRef;
+
+  public diagramNodeData = [
+    { key: 1, header: 'Supplier', text: 'Planned Order Variations', footer: 'Retailer', role: 'b' },
+    { key: 2, header: 'Supplier', text: 'Order & Delivery Variations', footer: 'Retailer', role: 't', loop: true },
+    { key: 3, header: 'Group 1', isGroup: true, footer: 'Shipper', role: 'b' },
+    { key: 4, header: 'Supplier', text: 'Inner Node A', footer: 'Retailer', role: 'b', group: 3 },
+    { key: 5, header: 'Supplier', text: 'Inner Node B', footer: 'Retailer', role: 't', group: 3 }
+  ];
+
+
+  public diagramLinkData = [
+    { key: -1, from: 1, to: 2 },
+    { key: -2, from: 2, to: 3 },
+    { key: -3, from: 4, to: 5 }
+  ];
+
+  public stateData = {
+
+    diagramNodeData: [
+      { key: 1, header: 'Supplier', text: 'Planned Order Variations', footer: 'Retailer', role: 'b' },
+      { key: 2, header: 'Supplier', text: 'Order & Delivery Variations', footer: 'Retailer', role: 't', loop: true },
+      { key: 3, header: 'Group 1', isGroup: true, footer: 'Shipper', role: 'b' },
+      { key: 4, header: 'Supplier', text: 'Inner Node A', footer: 'Retailer', role: 'b', group: 3 },
+      { key: 5, header: 'Supplier', text: 'Inner Node B', footer: 'Retailer', role: 't', group: 3 }
+    ],
+
+    diagramLinkData: [
+      { key: -1, from: 1, to: 2 },
+      { key: -2, from: 2, to: 3 },
+      { key: -3, from: 4, to: 5 }
+    ],
+
+    diagramModelData: { prop: 'value' },
+    skipsDiagramUpdate: false,
+    selectedNodeData: [], // used by InspectorComponent
+
+  }
+
+  public observedDiagram! : any;
+
+
+  ngOnInit() {
+    this.initDiagram();
+  }
+
+  public initDiagram(): go.Diagram {
+    const $ = go.GraphObject.make;
+
+    const diagram = $(go.Diagram, this.myDiagramComponent.nativeElement, {
+      layout: $(go.TreeLayout, {
+        setsPortSpot: false,
+        setsChildPortSpot: false,
+        isRealtime: false
+      }),
+      'undoManager.isEnabled': true
+    });
+
+     diagram.groupTemplate = $(
+      go.Group, 'Vertical',
+      {
+        layout: $(go.TreeLayout, {
+          setsPortSpot: false,
+          setsChildPortSpot: false
+        }),
+        defaultStretch: go.GraphObject.Horizontal,
+        fromSpot: go.Spot.RightSide,
+        toSpot: go.Spot.LeftSide
+      },
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedRectangle', { fill: 'white', parameter2: 1 | 2 })
+          .bind('fill', 'role', r => r[0] === 't' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'header')
+      ),
+      $(go.Panel, 'Auto',
+        $(go.Shape, { fill: 'white' }),
+        $(go.Placeholder, { padding: 20 })
+      ),
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedRectangle', { fill: 'white', parameter2: 4 | 8 })
+          .bind('fill', 'role', r => r[0] === 'b' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'footer')
+      )
+    );
+
+    diagram.nodeTemplate = $(
+      go.Node, 'Vertical',
+      { defaultStretch: go.GraphObject.Horizontal, fromSpot: go.Spot.RightSide, toSpot: go.Spot.LeftSide },
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedTopRectangle', { fill: 'white' })
+          .bind('fill', 'role', r => r[0] === 't' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'header')
+      ),
+      $(go.Panel, 'Auto', { minSize: new go.Size(NaN, 70) },
+        $(go.Shape, 'Rectangle', { fill: 'white' }),
+        $(go.TextBlock, { width: 120, margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text')
+      ),
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedBottomRectangle', { fill: 'white' })
+          .bind('fill', 'role', r => r[0] === 'b' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'footer')
+      )
+    );
+
+    diagram.linkTemplate = $(
+      go.Link,
+      { routing: go.Routing.Orthogonal, corner: 5 },
+      $(go.Shape),
+      $(go.Shape, { toArrow: 'Triangle' })
+    );
+
+    const model = new go.GraphLinksModel(this.diagramNodeData, this.diagramLinkData);
+    model.linkKeyProperty = 'key';
+    diagram.model = model;
+
+    return diagram;
+  }
+
+
+    /*
+    diagram.groupTemplate = $(
+      go.Group, 'Vertical',
+      {
+        layout: $(go.GridLayout, { wrappingColumn: 1 }),
+        selectionObjectName: 'PANEL',
+        ungroupable: true
+      },
+      $(
+        go.Shape, 'RoundedTopRectangle',
+        {
+          fill: 'rgba(128,128,128,0.33)', stroke: '#999', height: 20, stretch: go.GraphObject.Horizontal
+        }
+      ),
+      $(
+        go.Panel, 'Auto',
+        { name: 'PANEL' },
+        $(go.Shape, 'RoundedBottomRectangle',
+          { fill: 'rgba(128,128,128,0.15)', stroke: '#999', strokeWidth: 1 }),
+        $(go.Placeholder, { padding: 10 })
+      )
+    );
+
+    diagram.nodeTemplate = $(
+      go.Node, 'Auto',
+      $(go.Shape, 'Ellipse', { fill: 'lightblue', stroke: null }),
+      $(go.TextBlock, { margin: 6 }, new go.Binding('text', 'key'))
+    );
+    */
+
+
+    /*
+    diagram.groupTemplate = $(
+      go.Group, 'Vertical',
+      {
+        layout: $(go.TreeLayout, {
+          setsPortSpot: false,
+          setsChildPortSpot: false
+        }),
+        defaultStretch: go.GraphObject.Horizontal,
+        fromSpot: go.Spot.RightSide,
+        toSpot: go.Spot.LeftSide
+      },
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedRectangle', { fill: 'white', parameter2: 1 | 2 })
+          .bind('fill', 'role', r => r[0] === 't' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'header')
+      ),
+      $(go.Panel, 'Auto',
+        $(go.Shape, { fill: 'white' }),
+        $(go.Placeholder, { padding: 20 })
+      ),
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedRectangle', { fill: 'white', parameter2: 4 | 8 })
+          .bind('fill', 'role', r => r[0] === 'b' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'footer')
+      )
+    );
+
+    diagram.nodeTemplate = $(
+      go.Node, 'Vertical',
+      { defaultStretch: go.GraphObject.Horizontal, fromSpot: go.Spot.RightSide, toSpot: go.Spot.LeftSide },
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedTopRectangle', { fill: 'white' })
+          .bind('fill', 'role', r => r[0] === 't' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'header')
+      ),
+      $(go.Panel, 'Auto', { minSize: new go.Size(NaN, 70) },
+        $(go.Shape, 'Rectangle', { fill: 'white' }),
+        $(go.TextBlock, { width: 120, margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text')
+      ),
+      $(go.Panel, 'Auto',
+        $(go.Shape, 'RoundedBottomRectangle', { fill: 'white' })
+          .bind('fill', 'role', r => r[0] === 'b' ? 'lightgray' : 'white'),
+        $(go.TextBlock, { margin: new go.Margin(2, 2, 0, 2), textAlign: 'center' })
+          .bind('text', 'footer')
+      )
+    );
+    */
+
+
+}
