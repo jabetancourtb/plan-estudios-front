941efc1d9de1a296b63be146086626be66616e28
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-20 00:10:25 -0500

    Extends page size options
    Adds "200" to the list of available page size options.
    
    This provides users with a larger page size option, potentially improving the efficiency of data browsing in certain scenarios.

--- src/app/dto/filter-pagination.model.ts
+++ src/app/dto/filter-pagination.model.ts
@@ -4,7 +4,7 @@ export class FilterPaginationDTO {
   pageSize: number = 10;
   totalItems: number = 0;
 
-  pageSizeOptions: number[] = [10, 25, 50, 100];
+  pageSizeOptions: number[] = [10, 25, 50, 100, 200];
   field: string  = '';
   fieldsOptions: any[] = [];
   ascending: boolean = true;
