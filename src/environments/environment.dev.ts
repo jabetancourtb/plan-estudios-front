4757f259cf2ab8402f4995bbdb3f09a87add71ef
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-20 01:30:26 -0500

    Removes API key and adds GoJs license key
    Removes the API key from the environment files
    for security reasons and adds a placeholder for
    the GoJs license key.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,10 +1,9 @@
 export const environment = {
-    production: false,
-    apiPlanEstudiosUDistrital:{
-        apiKey:{
-            requestHeader: "X-API-KEY",
-            requestValue: "H5sStbqNbvKL_29cnzGQFtgf_kWSWTkyjExFg1W_"
-        },
-        url: "http://localhost:9090/api/v1",
-    }
+  production: false,
+
+  apiPlanEstudiosUDistrital:{
+    url: "http://localhost:9090/api/v1",
+  },
+
+  GoJsLicenseKey: ''
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,9 @@
 export const environment = {
-    production: true,
-    apiPlanEstudiosUDistrital:{
-        apiKey:{
-            requestHeader: "X-API-KEY",
-            requestValue: "H5sStbqNbvKL_29cnzGQFtgf_kWSWTkyjExFg1W_"
-        },
-        url: "http://localhost:9090/api/v1",
-    }
+  production: true,
+
+  apiPlanEstudiosUDistrital:{
+    url: "http://localhost:9090/api/v1",
+  },
+
+  GoJsLicenseKey: ''
 }
