fd78b46547c6328d8c8d40e6a624386faf804981
Anderson Betancourt <jabetancourtb@hotmail.com>
2025-07-20 00:11:26 -0500

    Implements index page layout and navigation
    Sets up the index page with a grid layout for navigation cards.
    
    Adds navigation links to the different entities of the application,
    allowing users to easily access and manage them.
    
    The index page presents key entities as cards, each providing a brief
    description and a clear navigation path.

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,15 +1,55 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { APP_CONSTANTS } from '../../utils/app-constants';
 
 
 @Component({
   selector: 'app-index',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MatSlideToggleModule, MatCardModule, MatButtonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IndexComponent {
 
+  router = inject(Router);
+
+  menuOpen = false;
+
+
+  // Redirige a / luego redigecciona a la ruta deseada
+  goToCamposFormacionIndex() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([APP_CONSTANTS.ROUTES.camposFormacionIndex]);
+    });
+  }
+
+
+  // Redirige a / luego redigecciona a la ruta deseada
+  goToAreasFormacionIndex() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([APP_CONSTANTS.ROUTES.areasFormacionIndex]);
+    });
+  }
+
+
+   // Redirige a / luego redigecciona a la ruta deseada
+  goToAsignaturasIndex() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([APP_CONSTANTS.ROUTES.asignaturasIndex]);
+    });
+  }
+
+
+  // Redirige a / luego redigecciona a la ruta deseada
+  goToPlanEstudiosIndex() {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([APP_CONSTANTS.ROUTES.planEstudiosIndex]);
+    });
+  }
 
 }
